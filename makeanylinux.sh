eb3a31d46eac3c26fe4776700badebbd17c581bf
Yaroslav Polyakov <yaroslaff@gmail.com>
2024-06-27 08:21:25 +0700

    better style in makeanylinux.sh

--- makeanylinux.sh
+++ makeanylinux.sh
@@ -4,13 +4,13 @@ echo build anylinux version
 PYTHON=/opt/python/cp38-cp38/bin/python
 echo PYTHON=$PYTHON
 
-$PYTHON -m pip install wheel cython
+$PYTHON -m pip install wheel cython build
 
 echo +++ build sdist and wheel
-$PYTHON setup.py sdist bdist_wheel
-
+# $PYTHON setup.py sdist bdist_wheel
+$PYTHON -m build
 
 echo ++ fix wheels
-auditwheel repair dist/*whl
-
-ls -l dist/wheelhouse
+cd dist/
+auditwheel repair *whl
+ls -l wheelhouse
